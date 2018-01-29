dca2a48051fc003a34097f717fce8167298b5e74
Jonathan Lee <jonathanleeee@gmail.com>
2018-01-29 20:47:34 +0000

    add public and view dictories, also use body-parser to render data from MySQL back to home page

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var express    = require("express");
+var express    = require('express');
 var mysql      = require('mysql');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -8,13 +13,27 @@ var connection = mysql.createConnection({
   database : 'join_us'
 });
 
+
 app.get('/', function(req, res) {
    var q = 'SELECT COUNT(*) AS count FROM users';
    connection.query(q, function (error, results) {
       if (error) throw error;
-      var msg = 'We have ' + results[0].count + ' users';
-      res.send(msg);
+      var count = results[0].count;
+         res.render('home', {data: count});
     });
+});
+
+app.post('/register', function(req, res) {
+    // below code just show that incoming data will show in req.body after installing body-parser
+//   console.log('post request sent to /register ' + req.body.email); 
+   var person = {
+       email:req.body.email
+   };
+   connection.query('INSERT INTO users SET ?', person, function(error, result) {
+       if (error) throw error;
+       console.log(result);
+       res.redirect('/');
+   });
 });
 
 app.get('/joke', function(req, res) {
